e3760787e069f4482ca60cd020ce4e156d2b5da1
Castiel <castiel.andrew@gmail.com>
2018-03-10 18:18:02 -0800

    fixed init loading route

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Picker, StyleSheet } from 'react-native';
-import TaskMinsPicker from './Components/TaskMinsPicker.js'
+import TaskMinsPicker from '../components/TaskMinsPicker.js'
 export default class PickerExample extends Component {
   render() {
     return (<TaskMinsPicker />)
